78afeb7923fe4f4648c8c1c9f6465c1cd0dc156a
Caldas <fabio.caldas@vtex.com.br>
2019-09-22 21:04:28 -0300

    Start pressure test implementation

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -1,25 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Web.Configurations;
 
 namespace Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private static readonly Random random = new Random(DateTimeOffset.UtcNow.Millisecond);
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> logger;
+        private readonly ApplicationOptions applicationOptions;
+
+        public IndexModel(ILogger<IndexModel> logger, IOptions<ApplicationOptions> applicationOptions)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.applicationOptions = applicationOptions.Value;
+        }
+
+        [DisplayName("Anonymous")]
+        public string UserName { get; set; }
+
+        public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            var userCookieValue = Request.Cookies[applicationOptions.UserCookieName];
+            if (string.IsNullOrWhiteSpace(userCookieValue))
+            {
+                UserName = "Anonymous " + random.Next(0, 10000);
+                var cookieOptions = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddHours(2)
+                };
+                Response.Cookies.Append(applicationOptions.UserCookieName, UserName, cookieOptions);
+                logger.LogInformation($"New user created: `{UserName}`");
+            }
+            else
+            {
+                UserName = userCookieValue;
+                logger.LogInformation($"Old user returned: `{UserName}`");
+            }
+
+            base.OnPageHandlerExecuting(context);
         }
 
         public void OnGet()
         {
+            
+        }
 
+        public void OnPost(string newUserName)
+        {
+            if (!string.IsNullOrWhiteSpace(newUserName))
+            {
+                UserName = newUserName;
+            }
         }
     }
 }
